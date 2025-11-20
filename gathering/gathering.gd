d5b6aa048d3953e748e695e8df496ad72794f5ed
Drahomír Mrózek <drahosmrozek@gmail.com>
2025-11-20 18:55:00 +0100

    basic turn timer

--- gathering/gathering.gd
+++ gathering/gathering.gd
@@ -4,11 +4,15 @@ extends VBoxContainer
 var board = $HBoxContainer/Matching/Node2D/Match3Board
 @onready
 var board_container = $HBoxContainer/Matching
-
+@onready
+var timer = $TurnCounter/TurnLabel
 func _ready():
 	board.consumed_sequence.connect(Materials.add_matched_materials)
+	board.consumed_sequence.connect(add_energy_and_turn)
 	board_container.resized.connect(_resize_board)
-
+	board.movement_consumed.connect(update_timer)
+	update_timer()
+	
 func _resize_board():
 	var container_size_x = board_container.size[0]
 	var container_size_y = board_container.size[1]
@@ -20,6 +24,18 @@ func _resize_board():
 	var spare_space_horizontal = container_size_x - (cell_size * columns)
 	var spare_space_vertical = container_size_y -  (cell_size * rows)
 	# board seems to start in middle of first cell
-	board.set_position(Vector2((spare_space_horizontal + cell_size)/2, (spare_space_vertical+ cell_size)/2))
+	board.set_position(Vector2((spare_space_horizontal + cell_size)/2, (spare_space_vertical + cell_size)/2))
 	await board.draw_cells()
 	await board.draw_pieces()
+
+func add_energy_and_turn(sequence: Match3Sequence):
+	if sequence.pieces().size()>=5:
+		_add_extra_turn()
+	# TODO abilities
+
+func _add_extra_turn():
+	board.current_available_moves+=1
+	update_timer()
+	
+func update_timer():
+	timer.text="%s turns left"%board.current_available_moves
